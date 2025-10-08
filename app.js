89fea95c46bddace5435453b98f6afccf7ed0dc8
Smitayadav01 <960smitayadav@gmail.com>
2025-10-08 18:09:07 +0530

    committed

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/test";
 
@@ -32,10 +34,10 @@ app.get("/",(req,res)=>{
 })
 
 //index route
-app.get("/listings",async (req,res)=>{
+app.get("/listings", wrapAsync(async (req,res)=>{
     const allListings= await Listing.find({});
     res.render("listings/index",{allListings});
-})
+}))
 
 //new route
 app.get("/listings/new",(req,res)=>{
@@ -43,40 +45,46 @@ app.get("/listings/new",(req,res)=>{
 })
 
 //show route
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id", wrapAsync(async(req,res)=>{
     let{id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/show",{listing});
-})
+}))
 
 //create route
-app.post("/listings",async(req,res)=>{
+app.post("/listings",wrapAsync(async(req,res,next)=>{
+    if(!req.body.listing){
+        throw ExpressError(400,"Send valid data for listing");
+    }
     let newListing=new Listing(req.body.listing);
     await newListing.save();
-    res.redirect("/listings");
-})
+    res.redirect("/listings");  
+}))
 
 //edit route
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit", wrapAsync(async(req,res)=>{
     let{id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/edit",{listing});
-})
+}))
 
 //update route
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",  wrapAsync(async(req,res)=>{
+    if(!req.body.listing){
+        throw ExpressError(400,"Send valid data for listing");
+    }
     let{id}=req.params;
    await Listing.findByIdAndUpdate(id,{...req.body.listing});
    res.redirect(`/listings/${id}`);
-})
+}))
 
 //delete route
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let{id}=req.params;
    const del=await Listing.findByIdAndDelete(id);
    console.log(del);
    res.redirect("/listings");
-})
+}))
 
 // app.get("/testlisting",async (req,res)=>{
 //     let samplelistings=new Listing({
@@ -90,6 +98,15 @@ app.delete("/listings/:id",async(req,res)=>{
 //     console.log("Sample was saved");
 //     res.send("Sample listing is saved");
 // })
+
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found!"));
+})
+
+app.use((err,req,res,next)=>{
+    let{statusCode=500,message="Something went wrong"}=err;
+    res.status(statusCode).send(message);
+})
 
 app.listen(8080,()=>{
     console.log("Server is running on port 8080");
